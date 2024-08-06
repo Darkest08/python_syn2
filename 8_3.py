cbbceba7004903ec46b74a6609d397caee153892
Darkest08 <karpenkom2003@gmail.com>
2024-08-06 20:43:30 +0300

    add: 8_3

---
+++ 8_3.py
@@ -0,0 +1,25 @@
+boat_capacity = int(input())
+n_fishermen = int(input())
+fishermen = list()
+
+n_boats = 0
+prev_sum = 0
+
+for i in range(n_fishermen):
+    fishermen.append(int(input()))
+
+for i in range(n_fishermen):
+    if fishermen[i] != -1:
+        min_diff = boat_capacity
+        j_diff = i
+        for j in range(i+1, n_fishermen):
+            if fishermen[j] != -1:
+                sum = fishermen[i] + fishermen[j] 
+                if sum <= boat_capacity and sum > prev_sum:
+                    prev_sum = sum
+                    j_diff = j
+        
+        fishermen[i] = -1
+        fishermen[j_diff] = -1
+        n_boats+=1
+print(n_boats)
